f6af55d281b1d96ae3637f2a39b20a0135c406b1
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-24 23:15:21 +0300

    added Inheritance

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,15 +1,24 @@
-//package hexlet.code.schemas;
-//import java.util.Map;
-//
-//import java.util.LinkedHashMap;
-//
-//public class BaseSchema {
-//    private Map<String, String> attributes = new LinkedHashMap<>();
-//
-//    public boolean isValid(Object obj) {
-//        if (obj instanceof String) {
-//            return true;
-//        }
-//        return false;
-//    }
-//}
+package hexlet.code.schemas;
+import java.util.Collection;
+import java.util.Map;
+
+import java.util.LinkedHashMap;
+import java.util.function.Predicate;
+
+abstract class BaseSchema<T> {
+    private Map<String, Predicate<T>> attributes = new LinkedHashMap<>();
+
+    public boolean isValid(T obj) {
+//        System.out.println(attributes.size());
+        Collection<Predicate<T>> data = attributes.values();
+        if (obj == null && data.isEmpty()) {
+            return true;
+        } else if (obj == null) {
+            return false;
+        }
+        return data.stream().allMatch(x -> x.test(obj));
+    }
+    public void addCriteria(String name, Predicate<T> creterion) {
+        attributes.put(name, creterion);
+    }
+}
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,33 +1,28 @@
 package hexlet.code.schemas;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.Predicate;
-
-public class StringSchema {
-    private Map<String, Predicate<Object>> attributes = new LinkedHashMap<>();
+public class StringSchema extends BaseSchema<String> {
+//    private Map<String, Predicate<String>> attributes = new LinkedHashMap<>();
     public StringSchema required() {
-        attributes.put("required", x -> !x.toString().isEmpty());
+        addCriteria("required", x -> !x.isEmpty());
         return this;
     }
     public StringSchema minlength(int minLength) {
-        attributes.put("minLength", x -> x.toString().length() > minLength);
+        addCriteria("minLength", x -> x.length() > minLength);
         return this;
     }
     public StringSchema contains(String str) {
-        attributes.put("contains", x -> x.toString().contains(str));
+        addCriteria("contains", x -> x.contains(str));
         return this;
 
     }
 
-    public boolean isValid(Object obj) {
-        Collection<Predicate<Object>> data = attributes.values();
-        if (obj == null && attributes.isEmpty()) {
-            return true;
-        } else if (obj == null) {
-            return false;
-        }
-        return data.stream().allMatch(x -> x.test(obj));
-    }
+//    public boolean isValid(String obj) {
+//        Collection<Predicate<Object>> data = attributes.values();
+//        if (obj == null && attributes.isEmpty()) {
+//            return true;
+//        } else if (obj == null) {
+//            return false;
+//        }
+//        return data.stream().allMatch(x -> x.test(obj));
+//    }
 }
