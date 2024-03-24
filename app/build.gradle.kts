e8fcd008091f38b8995bbb985ea79fb8349c3462
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-24 19:32:08 +0300

    updated Tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
 
 tasks.test {
--- app/src/test/java/hexlet/code/StringSchemaTest.java
+++ app/src/test/java/hexlet/code/StringSchemaTest.java
@@ -1,4 +1,15 @@
 package hexlet.code;
 
+
+import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StringSchemaTest {
+    @Test
+    public void test1() {
+        Validator v = new Validator();
+        StringSchema schema = v.string();
+        assertTrue(schema.isValid(null));
+    }
 }
