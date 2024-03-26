2e94517d335848a530f37375548aa344db9ae57f
Eugene Popov <popoff.zhenya@yandex.ru>
2024-03-26 20:24:33 +0300

    updated MapSchemaTest

--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -3,6 +3,10 @@ package hexlet.code;
 import hexlet.code.schemas.MapSchema;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,5 +18,11 @@ public class MapSchemaTest {
         assertTrue(schema.isValid(null));
         schema.required();
         assertFalse(schema.isValid(null));
+        schema.sizeof(2);
+        Map<String, String> data = new HashMap<>();
+        data.put("key", "value");
+        assertFalse(schema.isValid(data));
+        data.put("key2", "value2");
+        assertTrue(schema.isValid(data));
     }
 }
